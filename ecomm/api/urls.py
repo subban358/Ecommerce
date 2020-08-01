e0d3da0fe417f7a3d91c4edb5ca7ac4772267e61
Subham <subhambanerjee138@gmail.com>
2020-08-01 10:55:36 +0530

    Category API urls routing and testing

---
+++ ecomm/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from rest_framework.authtoken import views
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='api.home'),
+    path('category/', include('api.category.urls'))
+]
