5d2e5c34ab2f872684047b531cf87d32bfc7d64b
subban358 <subhambanerjee138@gmail.com>
2020-09-09 12:14:15 +0530

    Orders urls added and registered in admin

--- ecomm/api/urls.py
+++ ecomm/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.authtoken import views
-from . import views
+from .views import home
 
 urlpatterns = [
-    path('', views.home, name='api.home'),
+    path('', home, name='api.home'),
     path('category/', include('api.category.urls')),
     path('product/', include('api.product.urls')),
     path('user/', include('api.user.urls')),
+    path('order/', include('api.order.urls')),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth')
+
 ]
