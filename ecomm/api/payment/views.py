00d21d82aa4b83488db1dbec39eaa488bb85d670
subban358 <subhambanerjee138@gmail.com>
2020-09-22 18:31:06 +0530

    process the payment in the backend

--- ecomm/api/payment/views.py
+++ ecomm/api/payment/views.py
@@ -33,3 +33,30 @@ def generate_token(request, id, token):
     if not validate_user_session(id, token):
         return JsonResponse({"error": "Invalid Session please login again !"})
     return JsonResponse({"clientToken": gateway.client_token.generate(), "success": True})
+
+@csrf_exempt
+def process_payment(request, id, token):
+    if not validate_user_session(id, token):
+        return JsonResponse({"error": "Invalid Session please login again !"})
+
+    nonce_from_the_client = request.form["paymentMethodNonce"]
+    amount_from_the_client = request.form["amount"]
+
+    result = gateway.transaction.sale({
+        "amount": amount_from_the_client,
+        "payment_method_nonce": nonce_from_the_client,
+        "options": {
+            "submit_for_settlement": True
+        }
+    })
+
+    if result.is_success:
+        return JsonResponse({
+            "success": result.is_success,
+            "transaction": {
+                'id': result.transaction.id,
+                'amount': result.transaction.amount
+            }
+        })
+    else:
+        return  JsonResponse({"error": True, "success": False})
